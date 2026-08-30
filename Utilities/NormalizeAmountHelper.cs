c77a0cec93c5c2272ee5ee32f9564d1b22524cab
Kelvinjaston <kelvinjaston@gmail.com>
2026-08-30 22:27:12 +0100

    Fix: track Utilities as regular files instead of a submodule

---
+++ Utilities/NormalizeAmountHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace Utilities
+{
+    public static class NormalizeAmountHelper 
+    {
+        public static decimal Normalize(decimal amount)
+        {
+            return Math.Abs(Math.Round(amount, 2, MidpointRounding.AwayFromZero));
+        }
+
+    }
+}
